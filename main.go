1d2efa2df6e7e473c8efe1ab15842a754dc68457
yoliveros <yoelvisoliveros@gmail.com>
2023-02-07 12:51:36 -0400

    background color test

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 type Game struct{}
@@ -16,6 +21,19 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0x00, 0x99, 0x00, 0xFF})
+	tt, err := opentype.Parse(fonts.PressStart2P_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	arcadeFont, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+
+	score := fmt.Sprintf("%04d", 25)
+	text.Draw(screen, score, arcadeFont, 640, 480, color.White)
 	ebitenutil.DebugPrint(screen, "Hello world!")
 }
 
@@ -25,7 +43,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello world!")
+	ebiten.SetWindowTitle("Snake")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
